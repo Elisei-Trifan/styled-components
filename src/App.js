130e460143edf0a255d4a0054e32a75a2e46d4c1
Elisei-Trifan <slavaberzan@gmail.com>
2025-01-24 23:51:22 +0200

    style css in js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: black;
+  padding: 2rem;
+`
+
 function App() {
-  return <></>
+  return <AppWrapper>app</AppWrapper>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+import styled, { createGlobalStyle } from 'styled-components'
+
+const Global = createGlobalStyle`
+* {
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+  font-family:consolas;
+  font-size:24px;
+}
+`
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>
+    <Global />
     <App />
   </>
 )
